c80b6d0216a72de660015536738ed5f1c95afca4
Anders Rundgren <anders.rundgren.net@gmail.com>
2024-02-25 16:55:28 +0100

    1.41 - new manifest location

--- app/src/main/java/org/webpki/mobile/android/application/PropertiesActivity.java
+++ app/src/main/java/org/webpki/mobile/android/application/PropertiesActivity.java
@@ -182,8 +182,8 @@ public class PropertiesActivity extends ListActivity {
                     version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
                 } catch (Exception e) {
                 }
-                about_builder.setMessage("This application was developed by PrimeKey " +
-                    "Solutions and WebPKI.org\n\nCurrent version: " + version);
+                about_builder.setMessage("This application was developed by " +
+                    "WebPKI.org\n\nCurrent version: " + version);
                 about_builder.setIcon(android.R.drawable.btn_star_big_on);
                 about_builder.setPositiveButton(android.R.string.ok,
                                                 new DialogInterface.OnClickListener() {
